8d4fd07dfd30ef9711fb074c6463c019e4ba2381
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-30 19:31:09 +0530

    j

--- src/App.js
+++ src/App.js
@@ -11,6 +11,31 @@ const Logo = () => {
         <h1 className="heading">HTML<span>&</span>CSS</h1>
         <p className="para">design and build websites</p>
       </div>
+      <div className="line green"></div>
+      <div className="line orange"></div>
+      <div className="line blue"></div>
+      <div className="line grey"></div>
+      <div className="text">
+        <h1 className="heading">HTML<span>&</span>CSS</h1>
+        <p className="para">design and build websites</p>
+      </div>
+      <div className="line green"></div>
+      <div className="line orange"></div>
+      <div className="line blue"></div>
+      <div className="line grey"></div>
+      <div className="text">
+        <h1 className="heading">HTML<span>&</span>CSS</h1>
+        <p className="para">design and build websites</p>
+      </div>
+      <div className="line green"></div>
+      <div className="line orange"></div>
+      <div className="line blue"></div>
+      <div className="line grey"></div>
+      <div className="text">
+        <h1 className="heading">HTML<span>&</span>CSS</h1>
+        <p className="para">design and build websites</p>
+      </div>
+      
     </div>
   );
 };
