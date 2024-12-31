38561582790afe106526d9423bdc64be970e76c0
dearguru07 <pangalaguruyadav101@gmail.com>
2024-12-31 20:38:13 +0530

    kki

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import Logo from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Logo />
+    <>
+    <Logo/>
+    </>
   </React.StrictMode>
 );
 
