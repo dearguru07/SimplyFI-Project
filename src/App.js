81cd7aefa3429f36bcb4728524d85bcf07e47026
dearguru07 <pangalaguruyadav101@gmail.com>
2025-03-06 09:22:59 +0530

    vite

--- src/App.js
+++ src/App.js
@@ -3,14 +3,7 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="logo-container">
-      <div className="line green"></div>
-      <div className="line orange"></div>
-      <div className="line blue"></div>
-      <div className="line grey"></div>
-      <div className="text">
-        <h1 className="heading">HTML<span>&</span>CSS</h1>
-        <p className="para">design and build websites</p>
-      </div>
+      <h4>Namstee Vite Bundler.....</h4>
     </div>
   );
 };
